2d6cbcc67f92722744a17d393937c288a83a5786
EPaetow <elipaetow@yahoo.com>
2020-09-07 19:47:30 -0500

    repo and service update

--- src/main/java/com/revature/repos/UserRepository.java
+++ src/main/java/com/revature/repos/UserRepository.java
@@ -1,5 +1,17 @@
 package com.revature.repos;
 
+import com.revature.models.Role;
+import com.revature.models.User;
+import com.revature.util.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
 public class UserRepository {
 
     //empty constructor
@@ -11,4 +23,95 @@ public class UserRepository {
             "JOIN project1.ers_user_roles er " +
             "ON eu.user_role_id = er.role_id ";
 
+
+
+    public Optional<User> findUserByCredentials(String username, String password) {
+
+        Optional<User> _user = Optional.empty();
+
+        /**
+         * Try with resources; the resource is the JDB
+         */
+        try (Connection conn = ConnectionFactory.getInstance().getConnection()) {
+
+            String sql = baseQuery + "WHERE username = ? AND password = ?";
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+
+            ResultSet rs = pstmt.executeQuery(); // assign the SQL query to a ResultSet object
+
+            /**
+             * Map the result set of the query to the _user Optional
+             */
+            _user = mapResultSet(rs).stream().findFirst();
+
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+
+        return _user;
+    }
+
+
+    public Optional<User> save(User newUser) {
+        try (Connection conn = ConnectionFactory.getInstance().getConnection()) {
+
+            String sql = "INSERT INTO project1.ers_users (username, password, first_name, last_name, email, user_role_id) " +
+                    "VALUES (?, ?, ?, ?, ?, ?)";
+
+            /**
+             * Prepared Statement uses user generated values, denoted with ?s
+             */
+            // second parameter here is used to indicate column names that will have generated values
+            PreparedStatement pstmt = conn.prepareStatement(sql, new String[] {"ers_user_id"});
+            pstmt.setString(1, newUser.getUsername());
+            pstmt.setString(2, newUser.getPassword());
+            pstmt.setString(3, newUser.getFirstName());
+            pstmt.setString(4, newUser.getLastName());
+            pstmt.setString(5, newUser.getEmail());
+            pstmt.setInt(6, newUser.getUserRole().ordinal() + 1);
+
+            int rowsInserted = pstmt.executeUpdate(); // returns an int that represents the #rows inserted
+
+            if (rowsInserted != 0) {
+
+                ResultSet rs = pstmt.getGeneratedKeys();
+
+                rs.next();
+                newUser.setUserId(rs.getInt(1));
+
+            }
+
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+
+        return null;
+
+    }
+    private Set<User> mapResultSet(ResultSet rs) throws SQLException {
+
+        Set<User> users = new HashSet<>();
+
+        /**
+         * Extract results, set the temporary AppUser fields, and add the temp AppUser to the Set.
+         */
+        while (rs.next()) {
+            User temp = new User();
+            temp.setUserId(rs.getInt("ers_user_id"));
+            temp.setUsername(rs.getString("username"));
+            temp.setPassword(rs.getString("password"));
+            temp.setFirstName(rs.getString("first_name"));
+            temp.setLastName(rs.getString("last_name"));
+            temp.setEmail(rs.getString("email"));
+            temp.setUserRole(Role.getByName(rs.getString("user_role_id")));
+            System.out.println(temp);
+            users.add(temp);
+        }
+
+        return users;
+
+    }
 }
